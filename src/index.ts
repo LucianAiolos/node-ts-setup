89ca90ca51294e0e62d9326c4c120bc2ce79c976
LuxianAiolos <luxianaiolos@gmail.com>
2023-02-10 17:33:56 -0800

    initialized

---
+++ src/index.ts
@@ -0,0 +1,42 @@
+// import { writeFile } from "fs";
+
+// writeFile
+
+// console.log('hello from ts hello again')
+
+import express from 'express'
+
+const app = express()
+
+// interface for objects
+// types for everything else
+
+type Params2 = {
+  a: number;
+  b: number;
+}
+
+interface Params {
+  a: number;
+  b: number;
+}
+
+// const add = (x: Params) => {
+//   return x.a + x.b
+// }
+
+const add = (a: number, b?: number) : number => {
+  if(b) {
+    return a + b
+  } else {
+    return a
+  }
+}
+
+app.get("/", req => {
+  add(1)
+})
+
+app.listen(9000, ()=> {
+  console.log("server running")
+})
